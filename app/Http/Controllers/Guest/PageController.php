daae9e0af3be645ef967861e60f5d660e96bd7aa
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-16 17:42:21 +0100

    printed db - cards without styles

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -13,7 +13,7 @@ class PageController extends Controller
     public function index()
     {
         $movies = Movie::all();
-        dd($movies);
+        // dd($movies);
         return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+    <h1>Movies</h1>
+ 
+    <div class="row justify-content-center gap-3 ">
+        @foreach ($movies as $movie)
+        <div class="col-2">
+                <div class="poster">
+                    <img src="{{$movie->image}}" class="card-img-top" alt="...">
+                </div>
+                <div class="card-info">
+                     <h3 class="title">{{$movie->title}}</h3>
+                     <div class="original-title">{{$movie->original_title}}</div>
+                     <div class="nationality">{{$movie->nationality}}</div>
+                     <div class="date">{{$movie->date}}</div>
+                     <div class="vote">{{$movie->vote}}</div>
+                </div>
+        </div>       
+        @endforeach
+    </div>
 @endsection
 
